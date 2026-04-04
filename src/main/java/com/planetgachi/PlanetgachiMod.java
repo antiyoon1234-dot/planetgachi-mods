dc3ab935ebc16d94554e3a2861fe8d7714bc209d
antiyoon_ <antiyoon1234@gmail.com>
2026-04-04 23:52:37 +0900

    Update PlanetgachiMod.java

--- src/main/java/com/planetgachi/PlanetgachiMod.java
+++ src/main/java/com/planetgachi/PlanetgachiMod.java
@@ -9,13 +9,15 @@ import org.slf4j.LoggerFactory;
 
 @Environment(EnvType.CLIENT)
 public class PlanetgachiMod implements ClientModInitializer {
-        com.planetgachi.command.CommandRegistrar.register();
 
     public static final String MOD_ID = "planetgachi";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
+        // ✅ 여기 안으로 이동
+        com.planetgachi.command.CommandRegistrar.register();
+
         PriceCommand.register();
         LOGGER.info("[Planetgachi] 모드 로드 완료 (v1.2.0)");
     }
