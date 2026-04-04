f63fe61e9b8e6da9f686f1711f0dd5dfa543749f
antiyoon_ <antiyoon1234@gmail.com>
2026-04-04 23:54:38 +0900

    Update CommandRegistrar.java

--- src/main/java/com/planetgachi/command/CommandRegistrar.java
+++ src/main/java/com/planetgachi/command/CommandRegistrar.java
@@ -1,7 +1,8 @@
-
 package com.planetgachi.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;   //  추가
+import com.mojang.brigadier.arguments.LongArgumentType;     //  추가
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
@@ -17,8 +18,8 @@ public class CommandRegistrar {
         dispatcher.register(
             ClientCommandManager.literal("gachi")
                 .then(ClientCommandManager.literal("vote")
-                    .then(ClientCommandManager.argument("item", net.minecraft.command.argument.StringArgumentType.string())
-                        .then(ClientCommandManager.argument("price", net.minecraft.command.argument.LongArgumentType.longArg(1))
+                    .then(ClientCommandManager.argument("item", StringArgumentType.string())   // ✅ 수정
+                        .then(ClientCommandManager.argument("price", LongArgumentType.longArg(1)) // ✅ 수정
                             .executes(VoteCommand::executeVote)
                         )
                     )
